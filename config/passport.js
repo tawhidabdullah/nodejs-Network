fef3ac17648590f2f8eb78ec23d16d986c791d5c
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-03-03 15:53:38 +0600

    Redux is Cool

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const keys = require('.././config/keys');
 
-const options = {};
+const options = {}; 
 
 options.jwtFromRequest = ExractJwt.fromAuthHeaderAsBearerToken(); // take token from header
 options.secretOrKey = keys.secretOrKey; // secret key 
@@ -15,9 +15,9 @@ module.exports = (passport) => {
       User.findById(jwt_payload.id).then((user) => {
         if(user){
           return done(null, user); // end the process with return the user ; 
-        }
+        }; 
         return done(null, false); 
-      })
+      }); 
     })
-  )
-}
+  ); 
+}; 
